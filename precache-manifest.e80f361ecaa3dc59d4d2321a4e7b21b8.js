bee4d40e52c844bf5b8efb4151c9bdfbc528ff06
Diwakarkirar <kirardiwakar540@gmail.com>
2022-07-20 12:50:27 +0530

    Updates

---
+++ precache-manifest.e80f361ecaa3dc59d4d2321a4e7b21b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567dd6ad4d71d68d65a0d59c997be919",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3e43fe6eba3d313157be",
+    "url": "/react-gh-pages/static/js/2.c21944f8.chunk.js"
+  },
+  {
+    "revision": "c9c70715ee4cad602b68",
+    "url": "/react-gh-pages/static/js/main.8bf7be41.chunk.js"
+  },
+  {
+    "revision": "94c4514f6443c252ae0f",
+    "url": "/react-gh-pages/static/js/runtime-main.d701a425.js"
+  }
+]);
